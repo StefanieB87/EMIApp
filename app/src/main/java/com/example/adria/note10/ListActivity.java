6de17cc45f4b965e897b6f78130890dd7d09c1ca
runa <runa>
2017-01-21 11:25:10 +0100

    adapter, die ich schon vorher geschrieben habe, integriert

--- app/src/main/java/com/example/adria/note10/ListActivity.java
+++ app/src/main/java/com/example/adria/note10/ListActivity.java
@@ -2,12 +2,19 @@ package com.example.adria.note10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
     ArrayList<Note> noteList = new ArrayList<Note>();
-    // TODO: Gui Elemente müssen noch hinzugefügt werden
+    private NoteAdapter noteAdapter;
+    private ListView noteListView;
+    private Button btnNewNote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +24,36 @@ public class ListActivity extends AppCompatActivity {
 
     private void InitializeApp(){
         //TODO: XML auslesen, noteList initialisieren
-        //TODO: GUI Elemente hier initialisieren
+        //TODO: GUI Elemente hier initialisieren (gui muss erstmal existieren)
+
+       //noteListView=(ListView)findViewById(R.id.noteList);
+        //btnNewNote=(Button)findViewById(R.id.NewNote);
+        Log.d("initializing", "NoteListActivity.java");
+        //create dummy note to display
+        Note note1=new Note();
+        Note note2 = new Note ();
+        note2.setTitle("Dies ist der Titel");
+        //Log.d("note.title", note.getTitle());
+
+        noteList = new ArrayList<Note>();
+        noteList.add(note1);
+        noteList.add(note2);
+
+        noteAdapter = new NoteAdapter(this, noteList);
+
+        noteListView.setAdapter(noteAdapter);
+
+        btnNewNote.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startNewActivity();
+            }
+        });
+
+        /*todo: onClickListener NoteListActivity -> starts editNote for the clicked note
+            problem: how do i get the pos of the clicked item??
+        */
+
     }
 
     private void startNewActivity(){
--- app/src/main/java/com/example/adria/note10/NoteAdapter.java
+++ app/src/main/java/com/example/adria/note10/NoteAdapter.java
@@ -1,22 +1,66 @@
 package com.example.adria.note10;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 /**
  * Created by adria on 19.12.2016.
+ * NoteAdapter connects the note to activity_edit gui
  */
 
 public class NoteAdapter extends ArrayAdapter<Note>{
     //TODO:GUI ELEMNTE
     private final Context context;
     private final ArrayList<Note> noteList;
+    private ImageView audioIcon;
+    private android.widget.ImageView textIcon;
+    private TextView titleView;
+
     public NoteAdapter(Context context, ArrayList<Note> noteList) {
         super(context, R.layout.list_row);
         this.context = context;
         this.noteList = noteList;
 
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        //create inflater
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+        //get row view from infltaer
+        View rowView = inflater.inflate(R.layout.list_row, parent, false);
+
+        //get fields from XML to be filled with data
+        //in the constructor, list_row is passed as argument
+        //audioIcon = (ImageView) rowView.findViewById(R.id.audio);
+        textIcon = (ImageView) rowView.findViewById(R.id.text);
+        titleView = (TextView) rowView.findViewById(R.id.title);
+
+        //set content
+        /* TODO: 16.11.16 is it better to let the Note return icon/no icon or to do it like this
+            (Adapter only gets boolean information on whether audio/text exists and eventually grabs the icon itself)
+        */
+        if(noteList.get(position).hasAudio()){
+            //// TODO: this does not work
+            //audioIcon.setImageDrawable(R.drawable.recordedAudio);
+        };
+
+        if (noteList.get(position).hasText()){
+            // TODO: 16.11.16 this also does not work out jet, of course
+            //textIcon.setImageDrawable(R.drawable.text);
+        }
+
+        titleView.setText(noteList.get(position).getTitle());
+
+
+        // 5. return rowView
+        return rowView;
     }
 }
